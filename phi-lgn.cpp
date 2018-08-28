1b32fdb4887b2da1da7af69073c7e6debfe21e7f
Ali Koptan <ali.koptan0@gmail.com>
2018-08-28 18:21:16 +0200

    adding Phi(n)

---
+++ phi-lgn.cpp
@@ -0,0 +1,14 @@
+long long phi(long long n){
+
+    for (long long p = 2; p * p <= n; p++){
+        if (n % p == 0){
+            while (!(n%p))
+                n/=p;
+            result*=(1.0-(1.0/(double)p));
+        }
+    }
+    if (n > 1)
+        result*=(1.0-(1.0/(double)n));
+ 
+    return (long long)result;
+}
